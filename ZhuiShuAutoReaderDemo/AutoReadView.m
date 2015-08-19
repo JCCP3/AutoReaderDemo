c66f820bfe267ea4eac7cfd7b2b2d94e0dad1f07
JCCP3 <616613995@qq.com>
2015-08-19 10:31:05 +0800

    提交

--- ZhuiShuAutoReaderDemo/AutoReadView.m
+++ ZhuiShuAutoReaderDemo/AutoReadView.m
@@ -16,9 +16,6 @@
     UIView *nextShowView;
     
     BOOL isStop;
-    
-    UIPanGestureRecognizer *pan;
-    UITapGestureRecognizer *tap;
 
     NSMutableArray *viewsInPageArray;
     
@@ -27,6 +24,8 @@
     NSInteger currentPage;
     
     CGFloat timerSpeed;
+    
+    UIView *shadowView;
 }
 
 @end
@@ -55,7 +54,20 @@
 {
     currentPage = 0; //初始化显示第一页
     timerSpeed = 0.01;
-
+    
+    shadowView = [[UIView alloc] initWithFrame:CGRectMake(0, 10, [UIScreen mainScreen].bounds.size.width, 1)];
+    shadowView.backgroundColor = [UIColor blackColor];
+    shadowView.layer.shadowColor = [UIColor blackColor].CGColor;
+    shadowView.layer.shadowOffset = CGSizeMake(4, 4);
+    shadowView.layer.shadowOpacity = 0.8;
+    shadowView.layer.shadowRadius = 4;
+    [self addSubview:shadowView];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapEvent:)];
+    [self addGestureRecognizer:tapGesture];
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panWithTouchEvent:)];
+    [self addGestureRecognizer:panGesture];
 }
 
 - (void)reloadData:(NSString *)content
@@ -68,7 +80,6 @@
 
 - (void)createView
 {
-    
     for (int i=0; i<2; i++) {
         NSString *content = [viewsInPageArray objectAtIndex:i];
         UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
@@ -85,16 +96,15 @@
         if (i == 0) {
             textView.backgroundColor = [UIColor clearColor];
             currentView = view;
-//            currentView.backgroundColor = [UIColor yellowColor];
+            currentView.backgroundColor = [UIColor yellowColor];
         } else {
             textView.backgroundColor = [UIColor clearColor];
             view.frame = CGRectMake(0, 0, 0, 0);
             nextShowView = view;
-//            nextShowView.backgroundColor = [UIColor redColor];
+            nextShowView.backgroundColor = [UIColor whiteColor];
         }
     }
 }
-
 
 - (void)beginAutoRead
 {
@@ -127,20 +137,48 @@
     [self invalidateTimer];
 }
 
-- (void)panWithCurrentView:(UIPanGestureRecognizer *)recognizer point:(CGPoint)point
+#pragma mark UIGestureAction
+- (void)tapEvent:(UITapGestureRecognizer *)recognizer
 {
+    isStop = !isStop;
+    if (isStop) {
+        [self endAutoRead];
+    } else {
+        if (!timer) {
+            timer = [NSTimer scheduledTimerWithTimeInterval:timerSpeed target:self selector:@selector(changeCurrentViewFrame:) userInfo:nil repeats:YES];
+            [timer fire];
+        }
+    }
+}
+
+- (void)panWithTouchEvent:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint point = [recognizer translationInView:self];
+    
     if (recognizer.state == UIGestureRecognizerStateBegan) {
         [self invalidateTimer];
     }
     
-    nextShowView.frame = CGRectMake(0, 0, nextShowView.bounds.size.width, nextShowView.bounds.size.height+point.y);
+    if (currentPage != 0) {
+        if (currentPage %2 == 0) {
+            currentView.frame = CGRectMake(0, 0, currentView.bounds.size.width, currentView.bounds.size.height+point.y);
+            [shadowView setFrame:CGRectMake(0, CGRectGetMaxY(currentView.bounds)-1, CGRectGetWidth(shadowView.bounds), CGRectGetHeight(shadowView.bounds))];
+        } else {
+            nextShowView.frame = CGRectMake(0, 0, nextShowView.bounds.size.width, nextShowView.bounds.size.height+point.y);
+            [shadowView setFrame:CGRectMake(0, CGRectGetMaxY(nextShowView.bounds)-1, CGRectGetWidth(shadowView.bounds), CGRectGetHeight(shadowView.bounds))];
+        }
+    } else {
+        nextShowView.frame = CGRectMake(0, 0, nextShowView.bounds.size.width, nextShowView.bounds.size.height+point.y);
+        [shadowView setFrame:CGRectMake(0, CGRectGetMaxY(nextShowView.bounds)-1, CGRectGetWidth(shadowView.bounds), CGRectGetHeight(shadowView.bounds))];
+        
+    }
     
     if (recognizer.state == UIGestureRecognizerStateEnded) {
         timer = [NSTimer scheduledTimerWithTimeInterval:timerSpeed target:self selector:@selector(changeCurrentViewFrame:) userInfo:nil repeats:YES];
         [timer fire];
     }
     
-    [self.delegate setTranslationZeroWithGesture:recognizer];
+    [recognizer setTranslation:CGPointZero inView:self];
 }
 
 #pragma mark - NSTimer Action
@@ -157,7 +195,6 @@
         currentHeight = CGRectGetHeight(nextShowView.bounds);
     }
     
-    
     if (currentHeight >= [UIScreen mainScreen].bounds.size.height) {
         if (currentPage >= [viewsInPageArray count]-1) {
             //结束
@@ -168,16 +205,18 @@
             [self beginAutoRead];
         }
     } else {
-        
+        [self bringSubviewToFront:shadowView];
         if (currentPage != 0) {
             if (currentPage %2 == 0) {
                 [currentView setFrame:CGRectMake(0, CGRectGetMinY(currentView.frame), [[UIScreen mainScreen] bounds].size.width, CGRectGetHeight(currentView.bounds)+1)];
+                [shadowView setFrame:CGRectMake(0, CGRectGetMaxY(currentView.bounds)-1, CGRectGetWidth(shadowView.bounds), CGRectGetHeight(shadowView.bounds))];
             } else {
                 [nextShowView setFrame:CGRectMake(0, CGRectGetMinY(nextShowView.frame), [[UIScreen mainScreen] bounds].size.width, CGRectGetHeight(nextShowView.bounds)+1)];
+                [shadowView setFrame:CGRectMake(0, CGRectGetMaxY(nextShowView.bounds)-1, CGRectGetWidth(shadowView.bounds), CGRectGetHeight(shadowView.bounds))];
             }
-            
         } else {
             [nextShowView setFrame:CGRectMake(0, CGRectGetMinY(nextShowView.frame), [[UIScreen mainScreen] bounds].size.width, CGRectGetHeight(nextShowView.bounds)+1)];
+            [shadowView setFrame:CGRectMake(0, CGRectGetMaxY(nextShowView.bounds)-1, CGRectGetWidth(shadowView.bounds), CGRectGetHeight(shadowView.bounds))];
         }
     }
 }
@@ -195,6 +234,11 @@
         [self invalidateTimer];
         timer = [NSTimer scheduledTimerWithTimeInterval:speed target:self selector:@selector(changeCurrentViewFrame:) userInfo:nil repeats:YES];
     }
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    return self;
 }
 
 @end
--- ZhuiShuAutoReaderDemo/AutoReadViewController.m
+++ ZhuiShuAutoReaderDemo/AutoReadViewController.m
@@ -37,14 +37,7 @@
     autoReadView = [[AutoReadView alloc] init];
     autoReadView.delegate = self;
     [autoReadView reloadData:@"1\n\n\n\n 2\n\n\n 3,4\n\n\n\n 5\n\n\n 6,7\n\n\n\n 8\n\n\n 9,10\n\n\n\n 11\n\n\n 12"];
-    
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapEvent:)];
-    [self.view addGestureRecognizer:tapGesture];
-    
-    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panWithTouchEvent:)];
-    [self.view addGestureRecognizer:pan];
     [self.view addSubview:autoReadView];
-    
     
     UIView *footerView = [[UIButton alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height-45, [UIScreen mainScreen].bounds.size.width, 45)];
     [footerView setBackgroundColor:[UIColor redColor]];
@@ -72,9 +65,6 @@
     [fastBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [fastBtn addTarget:self action:@selector(onClickMoreFast) forControlEvents:UIControlEventTouchUpInside];
     [footerView addSubview:fastBtn];
-    
-    [self loadAllPageData];
-
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -83,70 +73,9 @@
     self.navigationController.navigationBarHidden = YES;
 }
 
-- (void)loadAllPageData
-{
-    for (int i = 0; i < [bookContentArray count]; i++) {
-        
-        NSString *content = [bookContentArray objectAtIndex:i];
-        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
-        view.clipsToBounds = YES;
-        view.tag = i+1;
-        UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(view.bounds), CGRectGetHeight(view.bounds))];
-        textView.font = [UIFont systemFontOfSize:18];
-        textView.scrollEnabled = NO;
-        textView.text = content;
-        textView.editable = NO;
-        if (i == 0) {
-            textView.backgroundColor = [UIColor clearColor];
-        } else {
-            textView.backgroundColor = [UIColor whiteColor];
-            view.frame = CGRectMake(0, 0, 0, 0);
-        }
-        [view addSubview:textView];
-        [viewArray addObject:view];
-    }
-    
-    
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-
-#pragma mark - UIGestureAction
-- (void)tapEvent:(UITapGestureRecognizer *)tap
-{
-    isStop = !isStop;
-    if (isStop) {
-        [autoReadView endAutoRead];
-    } else {
-        [autoReadView beginAutoRead];
-    }
-}
-
-- (void)panWithTouchEvent:(UIPanGestureRecognizer *)recognizer
-{
-    CGPoint point = [recognizer translationInView:self.view];
-    
-    [autoReadView panWithCurrentView:recognizer point:point];
-}
-
-#pragma mark - AutoReadViewDelegate
-- (NSInteger)numberOfPageInAutoReadView:(AutoReadView *)autoReadView
-{
-    return [viewArray count];
-}
-
-- (UIView *)autoReadView:(AutoReadView *)view viewForPageAtIndex:(NSInteger)index
-{
-    return viewArray[index];
-}
-
-- (void)setTranslationZeroWithGesture:(UIPanGestureRecognizer *)recognizer
-{
-    [recognizer setTranslation:CGPointZero inView:self.view];
 }
 
 #pragma mark - BtnAction
